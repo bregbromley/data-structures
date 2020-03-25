3152468644e35d06765e54ff62ba7b74808ab43e
Breg Bromley <bregbromley@gmail.com>
2020-03-25 17:22:02 -0600

    Completed functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,28 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var objArr = Object.keys(storage);
+    if (objArr.length === 0) {
+      var max = 0;
+    } else {
+      var max = objArr[objArr.length - 1];
+    }
+    storage[Number(max) + 1] = value;
   };
 
   someInstance.dequeue = function() {
+    console.log('storage', storage);
+    var objArr = Object.keys(storage);
+    var idx = Number(objArr[0]);
+    var current = storage[idx];
+    delete storage[idx];
+    console.log('stor:', storage);
+    console.log( 'current', current);
+    return current;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;

cede60a53fd1c0aaeeb42628278bd4b01b72e2ed
Breg Bromley <bregbromley@gmail.com>
2020-03-27 11:12:24 -0600

    completed LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+
+    if (this.head === null) {
+      this.head = newNode;
+    }
+    if (this.tail !== null) {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
   };
 
   list.removeHead = function() {
+
+    var temp = this.head;
+    this.head = this.head.next;
+    delete temp;
+    return temp.value;
   };
 
   list.contains = function(target) {
+    if (this.head === null) {
+      return false;
+    }
+    var temp = this.head;
+    while (temp.value !== target) {
+      temp = temp.next;
+      if (temp === null) {
+        return false;
+      }
+    }
+    return true;
   };
-
   return list;
 };
 
